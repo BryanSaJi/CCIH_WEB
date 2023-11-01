45a096a5fe76199e9f22f1dd271d99af524c2141
Estebanm-97 <emoreira10507@ufide.ac.cr>
2023-11-01 17:23:01 -0600

    Visual fix

--- CCIH/assets/Administration/js/DataTable/Datatable.js
+++ CCIH/assets/Administration/js/DataTable/Datatable.js
@@ -3,16 +3,9 @@
 
 $(document).ready(function () {
     $('#example').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'excel', 'pdf', 'print'
-        ],
-        responsive: true
-    });
-});
-
-$(document).ready(function () {
-    $('#eample').DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        },
         dom: 'Bfrtip',
         buttons: [
             'excel', 'pdf', 'print'
