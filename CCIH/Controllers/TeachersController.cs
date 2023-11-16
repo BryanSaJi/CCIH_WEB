8da3833f8cbc9c66be77dc5da5448f4a5bebb4f0
Sasafg <saryfg@gmail.com>
2023-11-16 11:24:41 -0600

    act
    base para profesores, front.

---
+++ CCIH/Controllers/TeachersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCIH.Controllers
+{
+    public class TeachersController : Controller
+    {
+        // GET: Teachers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateTeacher()
+        {
+            return View();
+        }
+
+        public ActionResult RequestTeacher()
+        {
+            return View();
+        }
+
+        public ActionResult CheckInTeacher()
+        {
+            return View();
+        }
+        public ActionResult CoursesTeacher()
+        {
+            return View();
+        }
+    }
+}
