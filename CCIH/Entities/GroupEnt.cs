cc7ff077641548655d7f71c6d6ea02288fd4fa79
BryanSaJi <bsanabriaj@hotmail.com>
2023-11-19 13:19:04 -0600

    Revert "Cambios para entorno de trabajo"
    This reverts commit 3830a2ddc376c315ccea615b353e22a4e6f13c42.

--- CCIH/Entities/GroupEnt.cs
+++ CCIH/Entities/GroupEnt.cs
@@ -8,19 +8,6 @@ namespace CCIH.Entities
     public class GroupEnt
     {
         public long GroupId { get; set; }
-        public string GroupName { get; set; }
-        public int StudentsNumber { get; set; }
-        public int MaxStudentsNumber { get; set; }
-        public long CourseId { get; set; }
-        public long ScheduleId { get; set; }
-        public long TeacherId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
         public int StatusId { get; set; }
-        public string CourseName { get; set; }
-        public string TeacherName { get; set; }
-        public string ScheduleDescription { get; set; }
-
-
     }
 }
