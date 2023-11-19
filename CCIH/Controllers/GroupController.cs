3830a2ddc376c315ccea615b353e22a4e6f13c42
BryanSaJi <bsanabriaj@hotmail.com>
2023-11-19 13:07:14 -0600

    Cambios para entorno de trabajo

---
+++ CCIH/Controllers/GroupController.cs
@@ -0,0 +1,32 @@
+﻿using CCIH.Entities;
+using CCIH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCIH.Controllers
+{
+    public class GroupController : Controller
+    {
+
+        GroupModel modelGroup = new GroupModel();
+
+        // GET: Group
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult ShowGroups()
+        {
+            var Data = modelGroup.RequestGroupScrollDown();
+
+            return View(Data);
+        }
+    }
+}
--- CCIH/Models/GroupModel.cs
+++ CCIH/Models/GroupModel.cs
@@ -30,5 +30,7 @@ namespace CCIH.Models
             }
 
         }
+
+
     }
 }
